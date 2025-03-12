3c1773e1f827c3c5751dbe9164f22b0a35502541
Anurag-Chitnis <anuragchitnis86@gmail.com>
2025-03-11 23:06:06 -0700

    implemented stack using array

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,69 @@
-class Stack { 
-    //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+// Time Complexity:
+// Space Complexity: O(N)
+// Did this code successfully run on Leetcode : N/A
+// I don't see leetcode link to this problem 
+// Any problem you faced while coding this:
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
-    } 
-  
-    int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    } 
-  
-    int peek() 
-    { 
-        //Write your code here
-    } 
-} 
-  
+// Your code here along with comments explaining your approach
+class Stack {
+
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack 
+
+    Stack() {
+        top = -1;
+    }
+
+    boolean isEmpty() {
+        return top == -1;
+    }
+
+    boolean isFull() {
+        return top == MAX - 1;
+    }
+
+    boolean push(int x) {
+        if (isFull()) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        top++;
+        a[top] = x;
+        return true;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+
+        int temp = a[top];
+        top -= 1;
+        return temp;
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            System.out.println("Empty Stack");
+            return -1;
+        }
+
+        return a[top];
+    }
+;
+
+}
+
 // Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+class Main {
+
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }

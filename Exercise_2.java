b5fed326b1bd002f8d3585afc16c37bd4eef7e45
Anurag-Chitnis <anuragchitnis86@gmail.com>
2025-03-12 17:39:05 -0700

    implemented stack using linked list

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,52 +1,78 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
-    } 
-  
-    public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
-    public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
-    } 
-} 
+// Time Complexity: O(1)
+// isEmpty() - Checks if stack is empty (O(1))
+// push() - Adds element to top of stack (O(1))
+// pop() - Removes and returns top element (O(1))
+// peek() - Returns top element without removing it (O(1))
+// Space Complexity: O(N)
+// Did this code successfully run on Leetcode : N/A
+// I don't see leetcode link to this problem
+// Any problem you faced while coding this: NO
+
+public class StackAsLinkedList {
+
+    StackNode root;
+
+    static class StackNode {
+
+        int data;
+        StackNode next;
+
+        StackNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void push(int data) {
+        // Create temp node to with input data
+        StackNode temp = new StackNode(data);
+        if (isEmpty()) {
+            // if the stack is empty which means root will be the new node that get's added
+            root = temp;
+        } else {
+            // newNode or temp should point to the root and root should be updated to the top element
+            temp.next = root;
+            root = temp;
+        }
+    }
+
+    public int pop() {
+        // This is straight forward: check if stack is empty as the first operation could be pop
+        // if empty then print stack underflow otherwise
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        // Store the pooped element data which is the current root
+        // point temp to root.next as root would be top element
+        // update the root and return stored popped element
+        int poppedData = root.data;
+        StackNode temp = root.next;
+        root = temp;
+
+        return poppedData;
+    }
+
+    public int peek() {
+        // return root.data as root would be top element
+        return root.data;
+    }
+
+    //Driver code
+    public static void main(String[] args) {
+
+        StackAsLinkedList sll = new StackAsLinkedList();
+
+        sll.push(10);
+        sll.push(20);
+        sll.push(30);
+
+        System.out.println(sll.pop() + " popped from stack");
+
+        System.out.println("Top element is " + sll.peek());
+    }
+}
